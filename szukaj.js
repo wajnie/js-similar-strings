bf7d79f9d9082227de158cce0d1a85c7db4fe7de
Michał Waligóra <49998336+wajnie@users.noreply.github.com>
2019-04-26 21:49:41 +0200

    Add files via upload

---
+++ szukaj.js
@@ -0,0 +1,17 @@
+﻿const sm = require('string-similarity');
+let index = [];
+let list = [
+  "Horse",
+  "Hammer",
+  "Fish",
+  "Red",
+  "Sheep"
+  ]
+let word = "Fi"
+list.forEach(function(){
+  index.push(list);
+})
+
+ let match = sm.findBestMatch(word, list);
+ let target = match.bestMatch.target;
+console.log(target)
